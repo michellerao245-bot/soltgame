1bfa6901a7a570d8b0ddb35e9a958bb902b8c03d
Your Name <you@gmail.com>
2026-05-29 11:04:41 +0530

    Fixed MoonJump export path

--- src/features/index.js
+++ src/features/index.js
@@ -3,7 +3,7 @@ export { CyberDice } from './CyberDice/CyberDice';
 export { Dream11 } from './Dream11/Dream11';
 export { EmpireBattle } from './EmpireBattle/EmpireBattle';
 export { GhostHunter } from './GhostHunter/GhostHunter';
-export { Moonjump } from './Moonjump/Moonjump';
+export { MoonJump } from './MoonJump/MoonJump';
 export { NeoRoulette } from './NeoRoulette/NeoRoulette';
 export { PokerBaz } from './PokerBaz/PokerBaz';
 export { SoltCrash } from './SoltCrash/SoltCrash';
