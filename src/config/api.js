79c520164727a4fc7fb14aef5aa2b0c045fc5d8b
Your Name <you@gmail.com>
2026-05-30 10:39:36 +0530

    Connect ecogame backend

---
+++ src/config/api.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL = "https://ecobackend-two.vercel.app";
--- src/hooks/useWallet.jsx
+++ src/hooks/useWallet.jsx
@@ -1,5 +1,7 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
-import { injected } from 'wagmi/connectors';
+import { useEffect } from "react";
+import { API_BASE_URL } from "../config/api";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { injected } from "wagmi/connectors";
 
 export const useWallet = () => {
 
@@ -8,6 +10,29 @@ export const useWallet = () => {
   const { connect } = useConnect();
 
   const { disconnect } = useDisconnect();
+
+  useEffect(() => {
+  const createUser = async () => {
+    if (!isConnected || !address) return;
+
+    try {
+      await fetch(`${API_BASE_URL}/api/users/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          wallet: address,
+          username: "Guest",
+        }),
+      });
+    } catch (err) {
+      console.log("Create User Error:", err);
+    }
+  };
+
+  createUser();
+}, [address, isConnected]);
 
   const connectWallet = async () => {
     try {
